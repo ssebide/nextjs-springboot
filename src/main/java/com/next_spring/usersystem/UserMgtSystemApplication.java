29cb45e32536542c33515d78438f392bb2badb20
ssebide <sebiddejohn136@gmail.com>
2025-06-18 15:47:39 +0300

    Project Setup

---
+++ src/main/java/com/next_spring/usersystem/UserMgtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.next_spring.usersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgtSystemApplication.class, args);
+	}
+
+}
